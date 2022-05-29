2abc310f2090fbe15bb7ad3310a740a8f6176a36
super-shaman <danielfunicello@yahoo.com>
2022-05-29 11:39:40 -0400

    Added proper license for libnoise

--- VoxelChunk.cs
+++ VoxelChunk.cs
@@ -1648,6 +1648,30 @@ static int[] cornerIndexBFromEdge = new int[12]{
 public class worldNoise
 {
 
+    // noisegen.cpp
+    //
+    // Copyright (C) 2003, 2004 Jason Bevins
+    //
+    // This library is free software; you can redistribute it and/or modify it
+    // under the terms of the GNU Lesser General Public License as published by
+    // the Free Software Foundation; either version 2.1 of the License, or (at
+    // your option) any later version.
+    //
+    // This library is distributed in the hope that it will be useful, but WITHOUT
+    // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    // FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    // License (COPYING.txt) for more details.
+    //
+    // You should have received a copy of the GNU Lesser General Public License
+    // along with this library; if not, write to the Free Software Foundation,
+    // Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    //
+    // The developer's email is jlbezigvins@gmzigail.com (for great email, take
+    // off every 'zig'.)
+    //
+
+    // ported to C#, removed unused code
+    // removed all original comments
     const int X_NOISE_GEN = 1619;
     const int Y_NOISE_GEN = 31337;
     const int Z_NOISE_GEN = 6971;
